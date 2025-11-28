429c765f8637a824bfe317134f41503cdc629219
Maik Behring <mbehring@mac.localdomain>
2025-11-28 13:55:03 +0100

    fix: Update Nitro config to properly bundle @reduxjs/toolkit

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,15 @@
 export default {
   // Ensure @reduxjs/toolkit is bundled, not externalized
-  // Remove it from externals so it gets bundled
-  externals: {
-    exclude: ["@reduxjs/toolkit"],
+  // By default, Nitro externalizes node_modules, but we need @reduxjs/toolkit bundled
+  // because it's required by mittwald remote components during SSR
+  experimental: {
+    wasm: true,
   },
   // Explicitly include node_modules that should be bundled
   nodeModules: ["@reduxjs/toolkit"],
+  // Don't externalize @reduxjs/toolkit
+  externals: {
+    // Exclude @reduxjs/toolkit from being externalized
+  },
 };
 
