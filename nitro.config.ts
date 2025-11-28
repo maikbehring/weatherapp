69636146240a61da45e97bd48bc6542c4e939af4
Maik Behring <mbehring@mac.localdomain>
2025-11-28 13:35:34 +0100

    fix: Correct Nitro config syntax

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,11 +1,9 @@
-import { defineNitroConfig } from "nitropack/config";
-
-export default defineNitroConfig({
+export default {
   // Ensure @reduxjs/toolkit is bundled, not externalized
   externals: {
     inline: ["@reduxjs/toolkit"],
   },
   // Explicitly include node_modules that should be bundled
   nodeModules: ["@reduxjs/toolkit"],
-});
+};
 
